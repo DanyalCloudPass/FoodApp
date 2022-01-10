d80cc8582545090835fba35f853671af58c97f56
DanyalCloudPass <danyal.ali@cloudpass.com>
2022-01-10 13:37:19 +0000

    initial

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { outputAst } from "@angular/compiler";
+import { Component, Output, EventEmitter } from "@angular/core";
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+    @Output() featureSelected = new EventEmitter<string>();
+
+    collapsed = true;
+    onSelect(feature: string){
+        this.featureSelected.emit(feature);
+    }
+}
